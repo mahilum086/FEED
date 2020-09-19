93aabe283b57635eeeb2af57de1f1422fea734bc
marisamahilum@gmail.com <marisamahilum@gmail.com>
2020-09-19 14:41:17 +0800

    Commented the codes in streaming the video using url.

--- FEEDV1/Form1.cs
+++ FEEDV1/Form1.cs
@@ -18,34 +18,31 @@ namespace FEEDV1
         public Form1()
         {
             InitializeComponent();
-            ctr = 0;
-            stream = new MJPEGStream("http://192.168.10.86:4747/video");
-            stream.NewFrame += Stream_NewFrame;
+            //ctr = 0;
+            //stream = new MJPEGStream("http://192.168.10.86:4747/video");
+            //stream.NewFrame += Stream_NewFrame;
         }
 
         private void Stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
             pictureBox1.Image = bmp;
-            /*using (Image img = Image.FromFile(@"C:\path\to\img.jpg"))
-            {
-                if (img.RawFormat.Equals(System.Drawing.Imaging.ImageFormat.Jpeg))
-                {
-                    // ...
-                }
-            }*/
-            bmp.Save(@"C:\Users\mmb\Downloads\DIT\Project Management\FEED Files\images\frame_"+ ++ctr +".jpg");
-         
+
         }
 
         private void BtnStart_Click(object sender, EventArgs e)
         {
-            stream.Start();
+            //stream.Start();
         }
 
         private void BtnStop_Click(object sender, EventArgs e)
         {
-            stream.Stop();
+            //stream.Stop();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            //pictureBox2.Image = (Bitmap)pictureBox1.Image.Clone();
         }
     }
 }
